cd8a2371eed417b8dd2e437a6f1344e18a236dda
ChauLe19 <chaunble01@gmail.com>
2022-10-08 23:39:25 -0400

    Fix saving images & save collection mosaic cell images

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+from re import I
 from PIL import Image #python image library
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -6,6 +7,7 @@ import sys
 
 #Path to save-images
 IMAGES_FOLDER = os.path.join(os.getcwd(),"save-images")
+COLLECTION_FOLDER = os.path.join(os.getcwd(),"collection-images")
 
 #Set up Flask:
 app = Flask(__name__)
@@ -23,14 +25,13 @@ def postME():
     return "receiving " + str(request.get_json())
 
 
-@app.route('/upload-image', methods=['POST'])
+@app.route('/generate', methods=['POST'])
 def handleUploadingImage():
-    print(IMAGES_FOLDER)
     files = request.files
     file = files.get('file')
-    #file = request.files['file']
+
     # handle image here
-    #TODO
+    # TODO
     #1. Check that file is an image file
     #2. Convert image to one type - PNG? (if needed)
     #3. Rename image? (if needed)
@@ -50,14 +51,17 @@ def handleUploadingImage():
         return jsonify({ 'success': False, 'file': 'No File'})
 
     #check there is an image file
-    if (checkFileType(file.filename)):
-
+    if (checkFileType(file.headers['Content-Type'])):
         #save image to folder
         file.save(os.path.join(IMAGES_FOLDER, file.filename)) # os.getcwd()
 
         #convert image and resave
         #img = Image.open(os.path.join(IMAGES_FOLDER, file.filename))
         #img.save(file.filename[:-3] + "png")
+        
+        for collection_file in request.files.getlist('collection[]'):
+            if (checkFileType(collection_file.headers['Content-Type'])):
+                file.save(os.path.join(COLLECTION_FOLDER, collection_file.filename)) # os.getcwd()
 
         return jsonify({ 'success': True, 'file': 'Received'})
     else:
@@ -66,10 +70,10 @@ def handleUploadingImage():
 
 
 #check images function
-def checkFileType(filename):
+def checkFileType(filetype):
     print("checking type")
-    allowedTypes = {'png', 'jpg', 'jpeg'}
-    if (filename[-3] in allowedTypes):
+    allowedTypes = {'image/png', 'image/jpg', 'image/jpeg'}
+    if (filetype in allowedTypes):
         return True
     return False
 
