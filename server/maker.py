e1530643f1215904ca6ccb193d61c5b067f17930
qholmess <qholmes@vt.edu>
2022-10-18 19:37:30 -0400

    I was able to get the imaging to find the avg color but organization of tiles are not correct

--- server/maker.py
+++ server/maker.py
@@ -1,42 +1,110 @@
 import numpy as np
 import os
-import cv2 #may need to install library on computer use link --> https://blog.finxter.com/how-to-install-opencv-on-pycharm/
-from PIL import Image # PIL is the Python Imaging Library
-
+import cv2  # mayneedtoinstalllibraryoncomputeruselink-->https://blog.finxter.com/how-to-install-opencv-on-pycharm/
+from PIL import Image  # PIListhePythonImagingLibrary
+import glob
+# fromscipyimportspatial
+import sys
 import matplotlib.pyplot as plt
 import math
 import copy
 import warnings
+from scipy import spatial
 
-directory ='/C:\Users\Que\Desktop\2022Fall_Classes\ECE4574_LargeScale\Proejct\MosaicImageMaker\server'
-os.chdir(directory) #the directory can change based on what server or folder we wanna use this is temporary
-imagename=" "
-img_color=cv2.imread(imagename) #this is the image in color
-cv2.imshow(img_color)
-# img=Image.open(filename)  could also use this if it is more helpful to go about using this for uploadign images in cv2 doesnt work
+
+def findhue(mainpath, tiles):
+    # resizing the the image to be smaller and than making it big pixelizing the image
+    main_img = Image.open(mainpath)
+    width = int(np.round(main_img.size[0] / tile_size[0]))
+    height = int(np.round(main_img.size[1] / tile_size[1]))
+
+    tiny_main = main_img.resize((width, height)) # resize main to the size of the tiles
+    tiny_main.show()
+    #Added this so the image could be better pixelated
+    pixel_main = tiny_main.resize(main_img.size,Image.Resampling.NEAREST)
+    pixel_main.show()
+
+    # calculate avg color for each tile
+    avg_colors = []
+
+    for tile in tiles:
+       #first breaks each pixel in a tile into (20x20 indexs with RGB truple values)
+        arry=np.array(tile)
+        avg_avg = arry.mean(axis=0).mean(axis=0)# get the average of the a row
+       # avg_avg=avg_color#a verages the colors of the row
+
+        avg_colors.append(avg_avg)
+
+    # makes a tree for our colors
+    tree = spatial.KDTree(avg_colors)
+
+    # Empty integer array to store indices of tiles
+    closest_tiles = np.zeros((width, height), dtype=np.uint32)
+
+    for i in range(width):
+        for j in range(height):
+            pixel = pixel_main.getpixel((i, j))  # Getthe pixel color at (i, j)
+            closest = tree.query(pixel)  # Returns (distance, index)
+            closest_tiles[i, j] = closest[1]  # We only need the index
+
+    return pixel_main, closest_tiles
+
+
+def organize(pixel_main, close_tiles): # Organizes the tiles to be drawn on the mosiac image
+    '''
+Takesthenewcoloredimagesandorganizesthemtobeinthecorrectpositonfor
+'''
+    width = int(np.round(pixel_main.size[0] / tile_size[0]))
+    height = int(np.round(pixel_main.size[1] / tile_size[1]))
+    mosaic = Image.new('RGB',pixel_main.size) # RGB is the type of the image will be if i wanted I could make it gray
+
+    w = tile_size[0]     # width of the tiles
+    h = tile_size[1]     # height of the tiles
+
+    for i in range(width):
+        for j in range(height):
+            x,y = i*w, j*h
+
+            index = close_tiles[i , j]
+
+            mosaic.paste(tiles[index],(x,y))
+
+    mosaic.save("mosaic.jpg")
+    mosaic.show()
+
+    return mosaic
+
+
+
+mainpath = '/Users/Que/Desktop/2022Fall_Classes/ECE4574_LargeScale/Proejct/MosaicImageMaker/server/river.jpg'
+# with Image.open("MeninDark.jpg") as mainimg:
+
+# mainimg.show()
+
+tile_imgs = []
+tile_size = (10, 10)
+# addstheimagesfromfolderintoalist
+for name in glob.glob(
+        '/Users/Que/Desktop/2022Fall_Classes/ECE4574_LargeScale/Proejct/MosaicImageMaker/server/test_images/*'):
+    print("here")
+    tile_imgs.append(name)
+
+tiles = []  # resizesalltheimagesinpicture
+for path in tile_imgs:  # lookintocropeyeforanotherwaytodoit
+    tile = Image.open(path)
+    tile = tile.resize(tile_size)
+    #tile.show() #for testing resize
+    tiles.append(tile)
+
+pixel_main,close_tiles = findhue(mainpath, tiles)
+mosaic_img = organize(pixel_main,close_tiles)
+"""
+def main(img,otherimgs):
+    #First we find the avg colors of the tile images and then find the closest color to it
+
+
+    main(mainimg, tiles)  # call to use the main image
+    return True
 
 
 """
-Maybe look at cropeye in hw1 and look at the picture an how you would crop a image if possible
-you could look trhough the corp main image and set a random image from folder to be that color  on it 
-may have to color change save the new images and then organize
-"""
-def findhue(main_img,other_imgs):
-    '''
-    This fuction should look through the main upload image and the folder with other imgaes
-    and goes through each group of pixels of main and sets a image from the list to be that color
-    the RGB should just change
-
-    Find the average color for the list of images
-    and find the average rgb for the pixels in main image in each pixel batch
-    '''
-
-    for o in other_imgs:
-
-
-def organize(main_img,other_imgs):
-    '''
-    Takes the new colored images and organizes them to be in the correct positon for
-
-
-    '''
