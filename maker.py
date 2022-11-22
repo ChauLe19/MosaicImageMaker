f81d83af98c896cd0df47cd41959042aa9d46b30
ChauLe19 <chaunble01@gmail.com>
2022-11-22 15:17:35 -0500

    mosaic works
    Main functionality works
    
    Fix tile size
    
    Fix
    
    clean up and works
    
    Clean up again
    
    convert png to jpg
    
    filter

--- maker.py
+++ maker.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-import cv2  # mayneedtoinstalllibraryoncomputeruselink-->https://blog.finxter.com/how-to-install-opencv-on-pycharm/
+import cv2  # may need to install library on computer use link-->https://blog.finxter.com/how-to-install-opencv-on-pycharm/
 from PIL import Image  # PIListhePythonImagingLibrary
 import glob
 # fromscipyimportspatial
@@ -11,6 +11,9 @@ import copy
 import warnings
 from scipy import spatial
 
+tile_size = (10, 10)
+tile_imgs = []
+tiles = []  # resizes all the images in picture
 
 def findhue(mainpath, tiles):
     # resizing the the image to be smaller and than making it big pixelizing the image
@@ -19,10 +22,10 @@ def findhue(mainpath, tiles):
     height = int(np.round(main_img.size[1] / tile_size[1]))
 
     tiny_main = main_img.resize((width, height)) # resize main to the size of the tiles
-    tiny_main.show()
+    # tiny_main.show()
     #Added this so the image could be better pixelated
     pixel_main = tiny_main.resize(main_img.size,Image.Resampling.NEAREST)
-    pixel_main.show()
+    # pixel_main.show()
 
     # calculate avg color for each tile
     avg_colors = []
@@ -43,17 +46,17 @@ def findhue(mainpath, tiles):
 
     for i in range(width):
         for j in range(height):
-            pixel = pixel_main.getpixel((i, j))  # Getthe pixel color at (i, j)
+            pixel = pixel_main.getpixel((i * tile_size[0], j * tile_size[1]))  # Get the pixel color at (i, j)
             closest = tree.query(pixel)  # Returns (distance, index)
             closest_tiles[i, j] = closest[1]  # We only need the index
 
     return pixel_main, closest_tiles
 
 
-def organize(pixel_main, close_tiles): # Organizes the tiles to be drawn on the mosiac image
+def organize(pixel_main, close_tiles, result_image_path): # Organizes the tiles to be drawn on the mosiac image
     '''
-Takesthenewcoloredimagesandorganizesthemtobeinthecorrectpositonfor
-'''
+    Takes the new colored images and organizes them to be in the correct positon for
+    '''
     width = int(np.round(pixel_main.size[0] / tile_size[0]))
     height = int(np.round(pixel_main.size[1] / tile_size[1]))
     mosaic = Image.new('RGB',pixel_main.size) # RGB is the type of the image will be if i wanted I could make it gray
@@ -65,39 +68,41 @@ Takesthenewcoloredimagesandorganizesthemtobeinthecorrectpositonfor
         for j in range(height):
             x,y = i*w, j*h
 
-            index = close_tiles[i , j]
+            index = close_tiles[i, j]
 
             mosaic.paste(tiles[index],(x,y))
 
-    mosaic.save("mosaic.jpg")
-    mosaic.show()
+    mosaic.save(result_image_path)
+    # mosaic.show()
 
     return mosaic
 
+def mosaic_pic(main_pic, density):
+    global tile_size
+    global tile_imgs
+    global tiles
+    mainpath = main_pic
+    # with Image.open("MeninDark.jpg") as mainimg:
+    # mainimg.show()
+    tile_size = (density, density)
+    tile_imgs.clear()
+    tiles.clear()
 
+    
+    # adds the images from folder into a list
+    for name in glob.glob(
+            './collection_images/*'):
+        tile_imgs.append(name)
 
-mainpath = '/Users/Que/Desktop/2022Fall_Classes/ECE4574_LargeScale/Proejct/MosaicImageMaker/server/river.jpg'
-# with Image.open("MeninDark.jpg") as mainimg:
+    for path in tile_imgs:  # look into crop eye for another way to do it
+        tile = Image.open(path)
+        tile = tile.resize(tile_size)
+        #tile.show() #for testing resize
+        tiles.append(tile)
 
-# mainimg.show()
-
-tile_imgs = []
-tile_size = (10, 10)
-# addstheimagesfromfolderintoalist
-for name in glob.glob(
-        '/Users/Que/Desktop/2022Fall_Classes/ECE4574_LargeScale/Proejct/MosaicImageMaker/server/test_images/*'):
-    print("here")
-    tile_imgs.append(name)
-
-tiles = []  # resizesalltheimagesinpicture
-for path in tile_imgs:  # lookintocropeyeforanotherwaytodoit
-    tile = Image.open(path)
-    tile = tile.resize(tile_size)
-    #tile.show() #for testing resize
-    tiles.append(tile)
-
-pixel_main,close_tiles = findhue(mainpath, tiles)
-mosaic_img = organize(pixel_main,close_tiles)
+    pixel_main,close_tiles = findhue(mainpath, tiles)
+    mosaic_img = organize(pixel_main,close_tiles, mainpath)
+    
 """
 def main(img,otherimgs):
     #First we find the avg colors of the tile images and then find the closest color to it
