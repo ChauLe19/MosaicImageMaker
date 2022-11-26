b932326e9d57c7a1529733d9e6f35d305a3c964e
Ethan Dingus <ethanld01@gmail.com>
2022-11-26 14:01:39 -0500

    Loading Spinner working, needs to be centered

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import { usePromiseTracker } from "react-promise-tracker";
-import LoadingSpinner from "./LoadingSpinner";
+//import LoadingSpinner from "./loadingSpinner/loadingSpinner";
+//import { AreaProvider } from 'react-area'; //install react-area
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const LoadingIndicator = props => {
-  const { promiseInProgress } = usePromiseTracker({area: props.area});
-  return (
-    promiseInProgress && <LoadingSpinner area="image"/>
-  )
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-    <LoadingIndicator/>
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
